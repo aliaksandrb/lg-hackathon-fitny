d0995fe18fe31320f416fd2fb34acf18e2f0ebf6
Aliaksandr Buhayeu <aliaksandr.buhayeu@gmail.com>
2015-12-05 10:22:44 +0300

    sorting guides by category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,6 +10,7 @@ class CategoriesController < ApplicationController
   # GET /categories/1
   # GET /categories/1.json
   def show
+    @guides = @category.guides
   end
 
   # GET /categories/new
--- app/helpers/guides_helper.rb
+++ app/helpers/guides_helper.rb
@@ -1,2 +1,5 @@
 module GuidesHelper
+  def guides_title
+    content_for(:guides_title) || "Promoted Guides"
+  end
 end
